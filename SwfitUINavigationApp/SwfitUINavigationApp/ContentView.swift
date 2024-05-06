bec2ced1aa62cc6302c8827616ae9fd63b792962
Anil Kumar Potlapally <786.anil.potlapally@gmail.com>
2024-05-06 21:53:18 +0530

    params with Navigation is working perfectly . just push the page in button for navigation with parameters . ( Code Below Is Highlighted ) .
      Button("Go to Page 1") {
                    path.append(MyNavigation(appView : .page1,params : Params(data: "Hello World"))) // Navigate to Page 1
                }
    
     var body: some View {
            NavigationStack(path: $path) {
                HomeView(path: $path)
                    .navigationDestination(for: MyNavigation<String>.self) { view in
                        switch view.appView {
                        case .home:
                            HomeView(path: $path)
                        case .page1:
                            Page1View(path:$path , params : view.params)
                        case .page2:
                            Page2View(path: $path)
                        }
                    }
            }
        }
    
       case .page1:
                            Page1View(path:$path , params : view.params)

--- SwfitUINavigationApp/SwfitUINavigationApp/ContentView.swift
+++ SwfitUINavigationApp/SwfitUINavigationApp/ContentView.swift
@@ -7,6 +7,63 @@
 
 import SwiftUI
 
+class Params<T: Equatable & Hashable >: Equatable, Hashable {
+    var data: T
+    
+    init(data: T) {
+        self.data = data
+    }
+    
+    static func == (lhs: Params<T>, rhs: Params<T>) -> Bool {
+        return lhs.data == rhs.data
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(data)
+    }
+}
+
+
+ // example route details
+class RouteDetails : Hashable , Equatable {
+    var id : String;
+    var details : String;
+    
+    init(id: String, details: String) {
+        self.id = id
+        self.details = details
+    }
+    
+    static func == (lhs: RouteDetails, rhs: RouteDetails ) -> Bool {
+        return lhs.id == rhs.id && lhs.details == rhs.details
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(details)
+    }
+}
+
+struct MyNavigation<T: Equatable & Hashable >: Equatable, Hashable  {
+    
+    public let appView : AppView;
+    var params : Params<T>;
+    
+//    init(appView: AppView , params : Params) {
+//        self.appView = appView ;
+//        self.params = params ;
+//    }
+    
+    static func == (lhs: MyNavigation, rhs: MyNavigation) -> Bool {
+           return lhs.appView == rhs.appView && lhs.params == rhs.params
+       }
+    
+    func hash(into hasher: inout Hasher) {
+           hasher.combine(appView)
+           hasher.combine(params)
+       }
+}
+
 enum AppView {
     case home
     case page1
@@ -14,17 +71,17 @@ enum AppView {
 }
 
 struct ContentView: View {
-    @State private var path: [AppView] = []
+    @State private var path: [MyNavigation<String>] = []
 
     var body: some View {
         NavigationStack(path: $path) {
             HomeView(path: $path)
-                .navigationDestination(for: AppView.self) { view in
-                    switch view {
+                .navigationDestination(for: MyNavigation<String>.self) { view in
+                    switch view.appView {
                     case .home:
                         HomeView(path: $path)
                     case .page1:
-                        Page1View(path: $path)
+                        Page1View(path:$path , params : view.params)
                     case .page2:
                         Page2View(path: $path)
                     }
@@ -34,13 +91,13 @@ struct ContentView: View {
 }
 
 struct HomeView: View {
-    @Binding var path: [AppView]
+    @Binding var path:[MyNavigation<String>]
     
     var body: some View {
         VStack {
             Text("Home View")
             Button("Go to Page 1") {
-                path.append(.page1) // Navigate to Page 1
+                path.append(MyNavigation(appView : .page1,params : Params(data: "Hello World"))) // Navigate to Page 1
             }
         }
      //   .navigationBarBackButtonHidden(true) // No back button on home
@@ -48,13 +105,19 @@ struct HomeView: View {
 }
 
 struct Page1View: View {
-    @Binding var path: [AppView]
+  
+    @Binding var path: [MyNavigation<String>];
     
+    var params: Params<String> // Additional parameters
+
+ 
+  
     var body: some View {
         VStack {
             Text("Page 1")
+            Text( params.data )
             Button("Go to Page 2") {
-                path.append(.page2) // Navigate to Page 2
+                path.append(MyNavigation(appView : .page2,params : Params(data: ""))) // Navigate to Page 2
             }
             Button("Go to Home") {
                 path = [] // Reset navigation stack to HomeView
@@ -64,7 +127,7 @@ struct Page1View: View {
 }
 
 struct Page2View: View {
-    @Binding var path: [AppView]
+    @Binding var path: [MyNavigation<String>]
     
     var body: some View {
         VStack {
