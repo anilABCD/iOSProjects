c97077bd15180791730c17bf96cdb085b2baf55e
Anil Kumar Potlapally <786.anil.potlapally@gmail.com>
2025-03-10 17:20:24 +0530

    loading more is working better. when only scrolled to bottom .

--- DatingAppScreens/DatingAppScreens/Tabs/LikesScreen/LikesScreen.swift
+++ DatingAppScreens/DatingAppScreens/Tabs/LikesScreen/LikesScreen.swift
@@ -1,7 +1,26 @@
-
-
-
 import SwiftUI
+
+
+struct LoadMoreItem: Identifiable {
+    let id: String
+}
+
+struct LoadMoreView: View {
+    let id: String // ✅ Unique identifier each time
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("Loading... \(id)")
+            ProgressView()
+                .padding()
+            Spacer()
+        }
+        .listRowBackground(Color.clear)
+        .listRowSeparator(.hidden)
+        .id(id) // ✅ Forces SwiftUI to create a fresh instance
+    }
+}
+
 
 struct LikesScreenView: View {
     
@@ -10,83 +29,245 @@ struct LikesScreenView: View {
     @EnvironmentObject private var tokenManger : TokenManager
     @EnvironmentObject private var themeManager : ThemeManager
     
+    @State private var loadMore: [LoadMoreItem] = [LoadMoreItem(id: "0")]
+
+   
+    @State var skip : Int = 0
+    @State private var isLoading = false
+    @State private var hasMoreData = true
     
+    @State  var lastItemID: String? = nil // Track last item's ID
+    @State private var scrollOffset: CGFloat = 0
+
+    @State var visibleItemID : String? = nil
+    
+    func loadMoreItems() {
+        guard !isLoading && hasMoreData else { 
+            print("Skip loading: isLoading = \(isLoading), hasMoreData = \(hasMoreData)")
+            return 
+        }
+        
+        print("Loading more items, current skip: \(skip)")
+        
+        Task {
+            do {
+                await try self.fetchLikes()
+            } catch {
+                isLoading = false
+                print("Error loading more likes:", error)
+            }
+        }
+       
+        print("New skip value: \(skip)")
+    }
     
     func fetchLikes() async throws {
+        guard !isLoading && hasMoreData else { return }
+        
+        isLoading = true
         
         let data:LikesEncodable? = nil;
         
-        let urlRequest = try createURLRequest(method : "GET" , baseURL: "\(tokenManger.localhost)/likes/get-likes", accessToken: tokenManger.accessToken , data: data, parameters: nil )
+        let urlRequest = try createURLRequest(method : "GET" , baseURL: "\(tokenManger.localhost)/likes/get-likes?skip=\(skip)", accessToken: tokenManger.accessToken , data: data, parameters: nil )
         
         let likesResponse : [LikesResponse] = try await fetchDataArray(from: urlRequest)
         
-        likes = likesResponse;
+        DispatchQueue.main.async {
+            if likesResponse.isEmpty {
+                hasMoreData = false
+            } else {
+                likes = likes + likesResponse
+                
+                skip = skip + 10 ;
+            
+                loadMore = [LoadMoreItem(id: skip.description)]
+                
+            }
+            isLoading = false
+        }
         
         print(likesResponse)
+    }
+    
+    
+    func setLastId(){
+        lastItemID = likes.last?.id
     }
     
     
     var body: some View {
         NavigationStack {
             VStack {
-         
-                        List {
-                            
-                            if ( likes.isEmpty){
-                                
-                                HStack {
-                                    
-                                    Text("No Likes to show")
-                                } .listRowBackground(themeManager.currentTheme.backgroundColor)
-                                    .listRowSeparatorTint(themeManager.currentTheme.navigationLinkColor)
+                ScrollViewReader { proxy in
+                    List {
+                        if (likes.isEmpty) {
+                            HStack {
+                                Text("No Likes to show")
                             }
-                            
-                            ForEach(likes) { like in  // Iterate over likes directly
-            
-                              
-                                
-                                NavigationLink(destination: OthersProfileView(profile: like.userFrom ?? nil , photoUrl: "\(tokenManger.serverImageURL)")) {
-                                    LikeItemView(like: like, photoURL: "\(tokenManger.serverImageURL)")
-                                        .onAppear {
-                                            if let index = likes.firstIndex(where: { $0.id == like.id }), index == likes.count - 5 {
-                                                //                                              loadMoreItems()
-                                                print ("load more")
-                                            }
-            
-                                        }
-                                } .listRowBackground(themeManager.currentTheme.backgroundColor)
-                                    .listRowSeparatorTint(themeManager.currentTheme.secondaryColor)
-                                // Custom
-            
-            
-                            }
-            
+                            .listRowBackground(themeManager.currentTheme.backgroundColor)
+                            .listRowSeparatorTint(themeManager.currentTheme.navigationLinkColor)
                         }
-                        .scrollContentBackground(.hidden) // Removes default background
+                        
+                        ForEach(likes) { like in
+                            NavigationLink(destination: OthersProfileView(profile: like.userFrom ?? nil, photoUrl: "\(tokenManger.serverImageURL)")) {
+                                LikeItemView(like: like, photoURL: "\(tokenManger.serverImageURL)")
+                            }
+                            .listRowBackground(themeManager.currentTheme.backgroundColor)
+                            .listRowSeparatorTint(themeManager.currentTheme.secondaryColor)
+                            .id(like.id) // Add id for scroll tracking
+                            
+                            .overlay(GeometryReader { geo in  // ✅ Use overlay instead of background
+                                Color.clear
+                                    .preference(key: VisibleItemKey.self, value: like.id)
+                            })
+                        }
+                        
+                        
+                        ForEach(loadMore) { item in
+                            LoadMoreView(id :item.id)
+                            .id(item.id)
+                            .overlay(GeometryReader { geo in
+                                
+                                DispatchQueue.main.async {
+                                               print("Dynamic overlay executed for \(item.id)")
+                                               
+                                    loadMoreItems()
+                                        
+                                           }
+                                
+                                return Color.clear
+                                    .preference(key: VisibleItemKey.self, value: item.id) // ✅ Tracks visibility
+                            })
+                        }
+                        
+                        if !likes.isEmpty && hasMoreData {
+     
+                            HStack {
+                                Spacer()
+                                Text("Loading\(skip)")
+                                
+                                if isLoading {
+                                    ProgressView()
+                                        .padding()
+                                    
+                                   
+                                }
+                                Spacer()
+                            }
+                            .id("loadMore-\(skip)")// ✅ Forces SwiftUI to recreate when `skip` changes
+                            .listRowBackground(themeManager.currentTheme.backgroundColor)
+                            .listRowSeparator(.hidden)
+                            .overlay(GeometryReader { geo in  // ✅ Use overlay instead of background
+                                Color.clear
+                                    .preference(key: VisibleItemKey.self, value: "loadMore-\(skip)")
+                            })
+                        }
+                        Color.clear.frame(height:100)
+                        
+                    }
+                    .scrollContentBackground(.hidden)
                     .background(themeManager.currentTheme.backgroundColor)
+                    .background(GeometryReader { geo in
+                                    Color.clear
+                                        .preference(key: ScrollOffsetKey.self, value: geo.frame(in: .global).minY)
+                                })
+                    .onPreferenceChange(ScrollOffsetKey.self) { newOffset in
+                               scrollOffset = -newOffset
+                               
+                        print("scroll changed")
+                           }
+//                    .background(GeometryReader { geo -> Color in
+//                        
+//                             
+//                        print("backgroundchanged")
+//                                    DispatchQueue.main.async {
+//                                        let contentHeight = geo.size.height
+//                                        let scrollOffset = geo.frame(in: .global).maxY
+//                                        let screenHeight = UIScreen.main.bounds.height
+//                                        print("for loading data entered")
+//                                        if scrollOffset < screenHeight + 50 && !likes.isEmpty && hasMoreData  { // Load when near bottom
+//                                            
+//                                            print("has more data ")
+//                                            
+//                                            if let lastItemID = lastItemID {
+//                                                if let id = likes.last?.id {
+//                                                    
+//                                                    if ( lastItemID != id){
+//                                                        
+//                                                        print("loadmore data called")
+//                                                        
+//                                                        loadMoreItems()
+//                                                        self.setLastId()
+//                                                    }
+//                                                    
+//                                                }
+//                                            }
+//                                            else {
+//                                                loadMoreItems()
+//                                                self.setLastId()
+//                                            }
+//                                            
+//                                         
+//                                        }
+//                                        print("for loading data exited")
+//                                    }
+//                                    return Color.clear
+//                                })
                     .listStyle(PlainListStyle())
-                   
-                    .accentColor(themeManager.currentTheme.primaryColor) // Global fallback
+                    .accentColor(themeManager.currentTheme.primaryColor)
+                 
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .navigationBarTitle("Likes", displayMode: .inline)
+            .background(themeManager.currentTheme.backgroundColor)
+             
+        }
+        .background(themeManager.currentTheme.backgroundColor)
+        .onPreferenceChange(ScrollOffsetKey.self) { newOffset in
             
-           }.frame(maxWidth: .infinity, maxHeight: .infinity) .navigationBarTitle("Likes" , displayMode: .inline) .background(themeManager.currentTheme.backgroundColor)
-        }.background(themeManager.currentTheme.backgroundColor) // Forces it to take the full screen
+            print("New offset:\(newOffset)")
+            
+                    scrollOffset = newOffset
+                    
+                }
+        .onPreferenceChange(VisibleItemKey.self) { newID in
+            visibleItemID = newID
+            print("visual item id \(visibleItemID)")
+            checkForLoadMore()
            
-        .onAppear(){
-            
+                  
+             }
+        .onAppear {
             updateNavigationBarColor()
-           
             Task {
                 do {
                     try await fetchLikes()
-                }
-                catch{
-                    
+                } catch {
+                    print("Error loading initial likes:", error)
                 }
             }
-           
         }
     }
     
+    private func checkForLoadMore() {
+        
+        
+        
+            if visibleItemID == "loadMore-\(skip)", hasMoreData, lastItemID != likes.last?.id {
+               
+                loadMoreItems()
+                lastItemID = likes.last?.id
+            }
+        }
+    
+    // ✅ PreferenceKey to track scroll offset
+    struct ScrollOffsetKey: PreferenceKey {
+        static var defaultValue: CGFloat = 0
+        static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+            value = nextValue()
+        }
+    }
     
     func updateNavigationBarColor() {
             let appearance = UINavigationBarAppearance()
@@ -101,7 +282,13 @@ struct LikesScreenView: View {
 }
 
 
-
+// ✅ PreferenceKey to Track Visible Items
+struct VisibleItemKey: PreferenceKey {
+    static var defaultValue: String? = nil
+    static func reduce(value: inout String?, nextValue: () -> String?) {
+        value = nextValue()
+    }
+}
 
 struct LikeItemView: View {
     let like: LikesResponse
